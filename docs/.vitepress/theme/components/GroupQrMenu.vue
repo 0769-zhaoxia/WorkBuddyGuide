b99a5e23b4e7ee6a6c843ea8fc22c81180bf8423
logCong <45169043+liucongg@users.noreply.github.com>
2026-08-07 19:36:33 +0800

    更新交流群二维码

--- docs/.vitepress/theme/components/GroupQrMenu.vue
+++ docs/.vitepress/theme/components/GroupQrMenu.vue
@@ -13,7 +13,7 @@ withDefaults(
   <figure class="wb-group-qr" :class="{ 'is-screen-menu': screenMenu }">
     <div class="wb-group-qr__frame">
       <img
-        src="/community/workbuddy-wechat-group.jpg?v=20260803-2"
+        src="/community/workbuddy-wechat-group.jpg?v=20260807"
         width="396"
         height="396"
         alt="WorkBuddy 微信交流群二维码"
